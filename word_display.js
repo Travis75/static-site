3151f8dac477152a4203978e032a7df24bb66dd6
Travis75 <travis.gibson75@gmail.com>
2015-08-31 21:26:37 -0700

    Got display clearing every time

--- word_display.js
+++ word_display.js
@@ -1,7 +1,8 @@
 $( document ).ready(function() {
-
+  CANVAS_WIDTH = window.innerWidth;
+  CANVAS_HEIGHT = window.innerHeight;
 //Create canvas with the device resolution.
-var myCanvas = createHiDPICanvas(window.innerWidth, window.innerHeight)
+var myCanvas = createHiDPICanvas(CANVAS_WIDTH, CANVAS_HEIGHT)
 $('body').append(myCanvas)
 
   var c = $('canvas')[0]
@@ -9,20 +10,34 @@ $('body').append(myCanvas)
   context.font = "30px Arial";
 
   var FPS = 30;
-  setInterval(function(context) {
-    drawText(context);
-  }(context), 1000/FPS);
+  setInterval(function(context, c) {
+    drawText(context, c);
+  }(context, c), 1000/FPS);
 });
 
 
 
-function drawText(context) {
+function drawText(context, canvas) {
+  context.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
   context.fillText("Hello World", 550, 100)
 }
 
 
 
-
+//  function fadeIn(text) {
+//    var alpha = 0,   // zero opacity
+//    interval = setInterval(function () {
+//      canvas.width = canvas.width; // Clears the canvas
+//      context.fillStyle = "rgba(255, 0, 0, " + alpha + ")";
+//      context.font = "italic 20pt Arial";
+//      context.fillText(text, 50, 50);
+//      alpha = alpha - 0.05; // decrease opacity (fade out)
+//      if (alpha < 0) {
+//        canvas.width = canvas.width;
+//        clearInterval(interval);
+//      }
+//    }, 50);
+//
 
 
 
