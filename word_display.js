ec0dbd2d0b4d268d79a8304a65933fb3a3e7f7db
Travis75 <travis.gibson75@gmail.com>
2015-09-07 14:14:52 -0700

    Finished except for div shudder

--- word_display.js
+++ word_display.js
@@ -1,16 +1,29 @@
 $( document ).ready(function() {
   var words = ["Pawtastic", "Pawdacious"]
-  $("#" + words.shift()).fadeIn(1500)
+  var words = $("#word-display img")
+  loop(words)
 });
 
-function WordPlay(words) {
-  this.word = words.shift();
-  this.words= words
+function loop(words) {
+  var word = words.shift();
+  var $word = $(word);
+  $word.fadeIn(2000);
+  $word.fadeOut(2000, function(){
+    words.push(word);
+    loop(words);
+  });
 }
 
-WordPlay.prototype = {
-  rotate: function() {
-    this.words.push(this.word)
-    this.word = words.shift();
-  }
-}
+(function( $ ) {
+    $.fn.pop = function() {
+        var top = this.get(-1);
+        this.splice(this.length-1,1);
+        return top;
+    };
+
+    $.fn.shift = function() {
+        var bottom = this.get(0);
+        this.splice(0,1);
+        return bottom;
+    };
+})( jQuery );
